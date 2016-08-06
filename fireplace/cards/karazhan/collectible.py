124b3cddfbae50a217b9d224805eb0f1529556c1
Kevin Skusek <nightkev@gmail.com>
2016-08-06 04:18:33 -0700

    Implement The Curator

--- fireplace/cards/karazhan/collectible.py
+++ fireplace/cards/karazhan/collectible.py
@@ -4,6 +4,19 @@ from ..utils import *
 ##
 # Minions
 
+class ONK_1:
+	"The Curator"
+	play = (
+		Find(FRIENDLY_DECK + BEAST) &
+		ForceDraw(RANDOM(FRIENDLY_DECK + BEAST))
+	), (
+		Find(FRIENDLY_DECK + DRAGON) &
+		ForceDraw(RANDOM(FRIENDLY_DECK + DRAGON))
+	), (
+		Find(FRIENDLY_DECK + MURLOC) &
+		ForceDraw(RANDOM(FRIENDLY_DECK + MURLOC))
+	)
+
 
 ##
 # Spells
